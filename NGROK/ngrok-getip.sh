4c40ff196880195e08f81780d2f381d4aecbec9d
Miraficus <63237821+miraficus@users.noreply.github.com>
2022-04-24 16:43:49 +0200

    Update ngrok-getip.sh
    fixed a mistake

--- NGROK/ngrok-getip.sh
+++ NGROK/ngrok-getip.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[] | .name, .public_url' | sed -e 's/\"//g' | cat > ip.txt
-sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' ip.txt | cat >> message.txt
+sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' ip.txt | cat > message.txt
 message="$(cat message.txt)"
 msg_content=\"$message\"
 url='https://replace-your-full-discord-webhoook-url-here'
